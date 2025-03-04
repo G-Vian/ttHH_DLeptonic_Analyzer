e18d48ef8c1b07367c38e7073e719e5116e2dbbc
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-03-04 14:58:57 -0300

    Update ttHHanalyzer_trigger.cc

--- ttHHanalyzer_trigger.cc
+++ ttHHanalyzer_trigger.cc
@@ -269,7 +269,7 @@ void ttHHanalyzer::createObjects(event * thisEvent, sysName sysType, bool up){
     if(!thereIsALeadLepton){
 	for(int i = 0; i < ele.size(); i++){
 	    if(fabs(ele[i].deltaEtaSC + ele[i].eta) < 1.4442 || fabs(ele[i].deltaEtaSC + ele[i].eta) > 1.5660){  //Electrons tracked neither in the barrel nor in the endcap are discarded.
-		if(fabs(ele[i].eta) < cut["eleEta"] && ele[i].cutBased == 4 && ele[i].pfRelIso03_all  < cut["eleIso"]){ 
+		if(fabs(ele[i].eta) < cut["eleEta"] && ele[i].cutBased == 3 && ele[i].pfRelIso03_all  < cut["eleIso"]){ 
 		    if(ele[i].pt > cut["leadElePt"]){
 			thereIsALeadLepton = true;
 			break;
@@ -296,7 +296,7 @@ void ttHHanalyzer::createObjects(event * thisEvent, sysName sysType, bool up){
 	}
 	for(int i = 0; i < ele.size(); i++){
 	    if(fabs(ele[i].deltaEtaSC + ele[i].eta) < 1.4442 || fabs(ele[i].deltaEtaSC + ele[i].eta) > 1.5660){  //Electrons tracked neither in the barrel nor in the endcap are discarded.
-		if(fabs(ele[i].eta) < cut["eleEta"] && ele[i].cutBased == 4 && ele[i].pfRelIso03_all  < cut["eleIso"]){ 
+		if(fabs(ele[i].eta) < cut["eleEta"] && ele[i].cutBased == 3 && ele[i].pfRelIso03_all  < cut["eleIso"]){ 
 		    if(ele[i].pt > cut["subLeadElePt"]){
 			currentEle = new objectLep(ele[i].pt, ele[i].eta, ele[i].phi, 0.);	 
 			currentEle->charge = ele[i].charge;
