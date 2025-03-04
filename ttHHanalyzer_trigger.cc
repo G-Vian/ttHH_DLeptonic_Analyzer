dd844aab32933cb925cc01435171e6763347651a
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-03-04 11:06:23 -0300

    Update ttHHanalyzer_trigger.cc

--- ttHHanalyzer_trigger.cc
+++ ttHHanalyzer_trigger.cc
@@ -296,12 +296,12 @@ void ttHHanalyzer::createObjects(event * thisEvent, sysName sysType, bool up){
 	}
 	for(int i = 0; i < ele.size(); i++){
 	    if(fabs(ele[i].deltaEtaSC + ele[i].eta) < 1.4442 || fabs(ele[i].deltaEtaSC + ele[i].eta) > 1.5660){  //Electrons tracked neither in the barrel nor in the endcap are discarded.
-		if(fabs(ele[i].eta) < cut["eleEta"] && ele[i].tightId == true && ele[i].pfRelIso04_all  < cut["eleIso"]){ 
+		if(fabs(ele[i].eta) < cut["eleEta"] && ele[i].tightId == true && ele[i].pfRelIso03_all  < cut["eleIso"]){ 
 		    if(ele[i].pt > cut["subLeadElePt"]){
 			currentEle = new objectLep(ele[i].pt, ele[i].eta, ele[i].phi, 0.);	 
 			currentEle->charge = ele[i].charge;
 			currentEle->miniPFRelIso = ele[i].miniPFRelIso_all;
-			currentEle->pfRelIso04 = ele[i].pfRelIso04_all;
+			currentEle->pfRelIso04 = ele[i].pfRelIso03_all;
 			thisEvent->selectEle(currentEle);
 		    }// else if(ele[i].pt > cut["vetoLepPt"]){	 
 		    //	nVetoEle++;
